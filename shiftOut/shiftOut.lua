3807c142cb3f01bc9de7ce60c1bee2f8d16098af
stefan-lochbrunner <stefan.lochbrunner@googlemail.com>
2015-08-24 15:53:57 +0200

    updated shiftOut

--- shiftOut/shiftOut.lua
+++ shiftOut/shiftOut.lua
@@ -1,52 +1,74 @@
--- function/lib to transfer data to shift register
--- based on https://github.com/hwiguna/g33k/tree/master/ArduinoProjects/RF/ESP8266/NodeMCU/Lua/ShiftReg
--- original rewritten into general purpose library
+-- module to transfer data to shift register
+-- S.L.
+-- 08/2015 initial version
+--
+-- based on
+--  - https://github.com/hwiguna/g33k/tree/master/ArduinoProjects/RF/ESP8266/NodeMCU/Lua/ShiftReg , original rewritten into general purpose library
+--  - https://www.arduino.cc/en/Reference/ShiftOut for MSBFIRST/LSBFIRST functionality
+--
 -- ToDo:
---  -implement bitOrder with string.reverse(s) and check
+--  - check bitOrder
+
+-- set up module
+local moduleName = ...
+local M ={}
+_G[moduleName] = M
 
 -- I/O configuration, initialized for ESP-01, change defualt here or call shiftOut with different pin config
-dataPin=3       --GPIO0
-clockPin=4      --GPIO2
-latchPin=5      --GPIO14
-gpio.mode(dataPin,gpio.OUTPUT)
-gpio.mode(latchPin,gpio.OUTPUT)
-gpio.mode(clockPin,gpio.OUTPUT)
+local dataPin = 3       --GPIO0
+local clockPin = 4      --GPIO2
+local latchPin = nil      --GPIO14
 
--- Initialize all to LOW --
-gpio.write(dataPin,gpio.LOW)
-gpio.write(latchPin,gpio.LOW)
-gpio.write(clockPin,gpio.LOW)
-
-function shiftOut(in_bits, in_bitOrder, in_dataPin, in_clockPin, in_latchPin)
-  --check for missing inputs, set to default if missing
-  if(not in_dataPin) then
-    in_dataPin = dataPin
-    in_clockPin = clockPin
+-- implementation
+--setup function, either call with or without paramters to use user specific or default pin config
+function setup(in_dataPin, in_clockPin, in_latchPin)
+  --check for inputs, leave as default if missing
+  if(in_dataPin) then
+    dataPin = in_dataPin
+    clockPin = in_clockPin
+    latchPin = in_latchPin
   end
+  --set as outputs
+  gpio.mode(dataPin,gpio.OUTPUT)
+  gpio.mode(latchPin,gpio.OUTPUT)
+
+  --initialize all to LOW --
+  gpio.write(dataPin,gpio.LOW)
+  gpio.write(latchPin,gpio.LOW)
+
+  --if in_latchPin is not defined assume shift register is unlatched
+  if(in_latchPin) then
+    gpio.mode(clockPin,gpio.OUTPUT)
+    gpio.write(in_latchPin,gpio.LOW)
+  end
+end
+
+function shiftOut(in_bits, in_bitOrder)
   --check for LSB or MSB first
-  if(in_bitOrder == LSB) then
+  if(in_bitOrder == 'LSB') then
     in_bits = tostring(in_bits)
   else
     in_bits = tostring(in_bits):reverse()
   end
+
   --check if latch pin was defined, omit latching if not defined
-  if(in_latchPin) then
-    gpio.write(in_latchPin,gpio.LOW)
+  if(latchPin) then
+    gpio.write(latchPin,gpio.LOW)
   end
   --clock out data
   for i = 1, 8 do
-    gpio.write(in_clockPin,gpio.LOW)
+    gpio.write(clockPin,gpio.LOW)
 
     if (string.sub(in_bits, i, i) == "0") then
-      gpio.write(in_dataPin,gpio.HIGH)
+      gpio.write(dataPin,gpio.HIGH)
     else
-      gpio.write(in_dataPin,gpio.LOW)
+      gpio.write(dataPin,gpio.LOW)
     end
 
-    gpio.write(in_clockPin,gpio.HIGH)
+    gpio.write(clockPin,gpio.HIGH)
   end
   --see above
-  if(in_latchPin) then
-    gpio.write(in_latchPin,gpio.HIGH)
+  if(latchPin) then
+    gpio.write(latchPin,gpio.HIGH)
   end
 end
--- shiftOut/shiftOut_example.lua
+++ shiftOut/shiftOut_example.lua
@@ -1,5 +1,9 @@
 --include 'library'
-require('shiftOut')
+sr = require('shiftOut')
 
---0
-shiftOut('11111100')
+--setup outputs
+sr.setup()  --use default pin confic for ESP-01
+
+--
+sr.shiftOut('11111100', 'LSB')
+sr.shiftOut('11111100', 'MSB')
